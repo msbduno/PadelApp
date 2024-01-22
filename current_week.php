2c16e80e7eeafefbf3b922bf902947f70b2adeef
Alexis Lopez <alexis.lopez2712@yahoo.fr>
2024-01-22 12:51:46 +0100

    trehert

--- current_week.php
+++
@@ -1,8 +0,0 @@
-<?php
-     $_SESSION['current_time'] = new DateTime();
-    $startDate = clone $_SESSION['current_time'];
-    print("<label for=week, class=week-label> " );
-    print($startDate->format('d/m/Y') . " to ");
-    $startDate->modify('+2 day');
-    print($startDate->format('d/m/Y') . "</label>");
-?>
